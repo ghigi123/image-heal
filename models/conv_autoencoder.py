827e8a866f829908025341917bd7bb58c5c32e27
Arnault Chazareix <arnault@chazareix.net>
2018-04-11 10:29:38 +0200

    improved kernel size

--- models/conv_autoencoder.py
+++ models/conv_autoencoder.py
@@ -7,37 +7,37 @@ class Autoencoder(nn.Module):
 
         self.encoder = nn.Sequential(
             # IN: 3 * 128 * 128 -> 49152
-            nn.Conv2d(3, 16, 4, stride=2, padding=1),
-            nn.BatchNorm2d(16),
-            # 16 * 64 * 64 -> 65536
+            nn.Conv2d(3, 32, 5, stride=2, padding=3),
+            nn.BatchNorm2d(32),
+            # 32 * 64 * 64 -> 131072
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=2),
-            # 16 * 32 * 32 -> 16384
-            nn.Conv2d(16, 8, 3, stride=2, padding=1),
-            nn.BatchNorm2d(8),
-            # 8 * 16 * 16 -> 2048
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2)
-            # CODE: 8 * 8 * 8 -> 512
-            # 1.04 %
-        )
-
-        self.decoder = nn.Sequential(
-            # CODE: 8 * 8 * 8 -> 512
-            # 1.04 %
-            nn.ConvTranspose2d(8, 16, 2, stride=2),
+            # 32 * 32 * 32 -> 32768
+            nn.Conv2d(32, 16, 4, stride=2, padding=2),
             nn.BatchNorm2d(16),
             # 16 * 16 * 16 -> 4096
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 2, stride=2),
-            nn.BatchNorm2d(8),
-            # 8 * 32 * 32 -> 8192
+            nn.MaxPool2d(2, stride=2)
+            # CODE: 16 * 8 * 8 -> 1024
+            # 2.08 %
+        )
+
+        self.decoder = nn.Sequential(
+            # CODE: 16 * 8 * 8 -> 1024
+            # 2.08 %
+            nn.ConvTranspose2d(16, 32, 4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            # 32 * 16 * 16 -> 8192
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 8, 2, stride=2),
+            nn.ConvTranspose2d(32, 16, 4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            # 16 * 32 * 32 -> 16284
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
             nn.BatchNorm2d(8),
             # 8 * 64 * 64 -> 32768
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 3, 2, stride=2),
+            nn.ConvTranspose2d(8, 3, 4, stride=2, padding=1),
             # OUT: 3 * 128 * 128 -> 49152
             nn.Tanh()
         )
