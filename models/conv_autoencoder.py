ea252e56be6f25c5d5af3a8194ba9f39fe4e570d
Arnault Chazareix <arnault.chazareix@gmail.com>
2018-04-13 12:17:16 +0200

    mmore accurate autoencoder

--- models/conv_autoencoder.py
+++ models/conv_autoencoder.py
@@ -1,47 +1,71 @@
 import torch.nn as nn
 
 
-class AutoEncoder(nn.Module):
-    def __init__(self, hyper_resolution=True):
-        super(AutoEncoder, self).__init__()
-        self.hyper_resolution = hyper_resolution
+def convolution(input_channels, output_channels, kernel_size, reduction=2):
+    pad = (kernel_size - reduction) // 2
+    if (kernel_size - reduction) % 2 == 0:
+        padl, padr = pad, pad
+    else:
+        padl, padr = pad, pad + 1
 
+    return nn.Sequential(
+        # assertion that reduction | input_size -> input_size / reduction = output_size int
+        nn.ReflectionPad2d((padl, padr, padl, padr)),
+        nn.Conv2d(input_channels, output_channels, kernel_size, reduction),
+        nn.ReLU(True),
+        nn.BatchNorm2d(output_channels),
+    )
+
+
+def deconvolution(input_channels, output_channels, kernel_size, augment=2):
+    pad = (kernel_size - augment) // 2
+    if (kernel_size - augment) % 2 == 0:
+        padl, padr = pad, pad
+    else:
+        padl, padr = pad, pad + 1
+
+    return nn.Sequential(
+        # assertion that reduction | input_size -> input_size / reduction = output_size int
+        nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride=augment, padding=pad),
+        nn.ReLU(True),
+        nn.BatchNorm2d(output_channels),
+    )
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_size=(3, 64), encoder_layers_params=None, decoder_layers_params=None, bn_size=1024):
+        # For layers the tuple represent (channels, kernel size)
+
+        super(Autoencoder, self).__init__()
+
+        if encoder_layers_params is None:
+            encoder_layers_params = [(32, 5), (64, 3), (128, 3), (256, 3)]
+
+        if decoder_layers_params is None:
+            decoder_layers_params = [(256, 4), (128, 4), (64, 4), (3, 4)]
+
+        n_convolutions = len(encoder_layers_params)
+
+        encoder_layers_params = [input_size] + encoder_layers_params
+        
         self.encoder = nn.Sequential(
-            # IN: 3 * 128 * 128 -> 49152
-            nn.Conv2d(3, 32, 5, stride=2, padding=3),
-            nn.BatchNorm2d(32),
-            # 32 * 64 * 64 -> 131072
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),
-            # 32 * 32 * 32 -> 32768
-            nn.Conv2d(32, 16, 4, stride=2, padding=2),
-            nn.BatchNorm2d(16),
-            # 16 * 16 * 16 -> 4096
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2)
-            # CODE: 16 * 8 * 8 -> 1024
-            # 2.08 %
+            *(convolution(encoder_layers_params[i][0], *encoder_layers_params[i + 1]) for i in range(n_convolutions))
         )
 
+        encoder_output_channels = encoder_layers_params[-1][0]
+
+        self.bottle_neck = nn.Sequential(
+            nn.Conv2d(encoder_output_channels, encoder_output_channels * bn_size, 1, groups=encoder_output_channels),
+            nn.ConvTranspose2d(encoder_output_channels * bn_size, encoder_output_channels, 1, groups=encoder_output_channels),
+            nn.ReLU(True)
+        )
+
+        n_deconvolutions = len(decoder_layers_params)
+
+        decoder_layers_params = [(encoder_output_channels, -1)] + decoder_layers_params
+
         self.decoder = nn.Sequential(
-            # CODE: 16 * 8 * 8 -> 1024
-            # 2.08 %
-            nn.Dropout2d(p=0.2),
-            nn.ConvTranspose2d(16, 32, 4, stride=2, padding=1),
-            nn.BatchNorm2d(32),
-            # 32 * 16 * 16 -> 8192
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, 4, stride=2, padding=1),
-            nn.BatchNorm2d(16),
-            # 16 * 32 * 32 -> 16284
-            nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
-            nn.BatchNorm2d(8),
-            # 8 * 64 * 64 -> 32768
-            nn.ReLU(True),
-            nn.ConvTranspose2d(8, 3, 4, stride=2, padding=1),
-            # OUT: 3 * 128 * 128 -> 49152
-            nn.Tanh()
+            *(deconvolution(decoder_layers_params[i][0], *decoder_layers_params[i + 1]) for i in range(n_deconvolutions))
         )
 
         # self.hr = nn.Sequential(
@@ -52,20 +76,15 @@ class AutoEncoder(nn.Module):
         #     nn.Tanh()
         # )
 
-        # self.nn = nn.Sequential(
-        #     self.encoder,
-        #     self.decoder#,
-        #     #self.hr
-        # )
+        self.nn = nn.Sequential(
+            self.encoder,
+            self.bottle_neck,
+            self.decoder
+        )
 
     def forward(self, input):
-        code = self.encoder(input)
-        output = self.decoder(code)
+        return self.nn(input)
 
-        #output = self.hr(output) + output
-
-        return output
-    
     def to_tune(self):
         return self
 
