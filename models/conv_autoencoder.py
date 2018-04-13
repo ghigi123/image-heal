f1cf4b331491e0e63368317672357c2a318caba6
Arnault Chazareix <arnault.chazareix@gmail.com>
2018-04-13 17:38:04 +0200

    cleaning

--- models/conv_autoencoder.py
+++ models/conv_autoencoder.py
@@ -32,11 +32,11 @@ def deconvolution(input_channels, output_channels, kernel_size, augment=2):
     )
 
 
-class Autoencoder(nn.Module):
+class AutoEncoder(nn.Module):
     def __init__(self, input_size=(3, 64), encoder_layers_params=None, decoder_layers_params=None, bn_size=1024):
         # For layers the tuple represent (channels, kernel size)
 
-        super(Autoencoder, self).__init__()
+        super(AutoEncoder, self).__init__()
 
         if encoder_layers_params is None:
             encoder_layers_params = [(32, 5), (64, 3), (128, 3), (256, 3)]
@@ -47,7 +47,7 @@ class Autoencoder(nn.Module):
         n_convolutions = len(encoder_layers_params)
 
         encoder_layers_params = [input_size] + encoder_layers_params
-        
+
         self.encoder = nn.Sequential(
             *(convolution(encoder_layers_params[i][0], *encoder_layers_params[i + 1]) for i in range(n_convolutions))
         )
