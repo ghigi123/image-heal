39842104ceecd41ce64bcaf54598f0f4096ede57
Arnault Chazareix <arnault.chazareix@gmail.com>
2018-06-20 23:28:39 +0200

    working best translation

--- prox_masking.py
+++ prox_masking.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from itertools import product
-
 SPATIAL_RESOLUTION = 4
 
 
@@ -40,9 +39,19 @@ def get_prox_op(image_size, kernel_size=25):
     return get_prox
 
 
-def _mse(searched_image, found_images):
-    return (searched_image - found_images).pow(2).sum(2).sum(2).sum(2)
+def _mse(searched_image, found_translations):
+    return (searched_image - found_translations).pow(2).sum(2).sum(2).sum(2)
 
+
+def mse_on_proximity(origin_image, translations, proximity_mask):
+    searched_image = origin_image.clone()
+    found_translations = translations.clone()
+
+    non_prox_idxs = proximity_mask != 1
+    searched_image[non_prox_idxs] = 0
+    found_translations[:, :, non_prox_idxs] = 0
+
+    return _mse(searched_image, found_translations)
 
 def build_translated(image, max_i, max_j):
     chan, height, width = image.size()
@@ -64,20 +73,23 @@ def build_translated(image, max_i, max_j):
 def best_translation(searched_image, found_image, proximity_mask):
     input_channels, width, height = searched_image.size()
 
-    non_prox_idxs = proximity_mask != 1
-
     block_width, block_height = width // SPATIAL_RESOLUTION, height // SPATIAL_RESOLUTION
     assert searched_image.size() == found_image.size()
 
     translations = build_translated(found_image, block_height, block_width)
 
-    found_image[non_prox_idxs] = 0
-    translations[:, :, non_prox_idxs] = 0
+    mse = mse_on_proximity(searched_image, translations, proximity_mask)
 
-    mse = _mse(searched_image, translations)
+    min_i, min_j = min(product(range(5), repeat=2), key=lambda t: mse[t])
+    return translations[min_i, min_j], (min_i, min_j)
 
-    return min(product(range(5), repeat=2), key=lambda t: mse[t])
-
+def dumb_seamcut(orig_scene, mask, match):
+    patched_scene = orig_scene.clone()
+    patched_scene[mask == 0] = 0
+    print(patched_scene)
+    print('=' * 77)
+    patched_scene[mask == 0] = match[mask == 0]
+    return patched_scene
 
 if __name__ == '__main__':
     from torchvision import transforms, utils as vutils
@@ -97,6 +109,10 @@ if __name__ == '__main__':
     vutils.save_image(limit, 'masking/limit.jpg')
 
     fake_image = torch.Tensor([[[i + j + k for j in range(20)] for i in range(20)] for k in range(3)])
+    fake_mask = torch.Tensor([[[i + j > 5 for j in range(20)] for i in range(20)] for k in range(3)])
 
+    best_image, (mi, mj) = best_translation(fake_image, fake_image + 5, fake_mask)
 
-    print(best_translation(fake_image, fake_image + 5, torch.Tensor([[[i + j > 5 for j in range(20)] for i in range(20)] for k in range(3)])))
+    print(best_image)
+
+    print(dumb_seamcut(fake_image, fake_mask, fake_image))
