f1eb28b2f766825efcd1a961a3f5963b31fd40f1
Arnault Chazareix <arnault.chazareix@gmail.com>
2018-06-21 01:49:45 +0200

    adding best translation to the im search

--- test_gist_search.py
+++ test_gist_search.py
@@ -1,4 +1,4 @@
-from gist import build_gist, get_masked_areas
+from gist import build_gist, get_masked_areas, build_gabor_conv
 from utils import parse_args, dataset_loaders, resolve
 import torch
 from time import time
@@ -8,6 +8,7 @@ from vector_store import VectorWriter, VectorReader
 import random as rd
 from struct import pack
 from shutil import copyfile
+from prox_masking import best_translation, get_prox_op
 
 
 def build_descriptor_database(data, descriptor, descriptor_size, filename):
@@ -63,25 +64,43 @@ if __name__ == '__main__':
             for i in range(20):
                 t0 = time()
 
-                new_idx = rd.randint(int(len(vr) * 0.8), len(vr) - 1)
-                mask = torch.ones(dataset.images[new_idx].size()[1:])
+                searched_image_idx = rd.randint(int(len(vr) * 0.8), len(vr) - 1)
+                searched_image = dataset.images[searched_image_idx]
+                mask = torch.ones(searched_image.size()[1:])
                 mask[:, :im_size[1] // 4, :] = 0
-                vutils.save_image(mask, 'default_mask.jpg')
                 assert get_masked_areas(mask) == [(0, 0), (0, 1), (0, 2), (0, 3)]
 
-                idxs = search(dataset.images[new_idx], blanks=get_masked_areas(mask))
-                paths = dataset.paths[[new_idx] + list(idxs[:40])]
-                for i, path in enumerate(paths):
-                    fn = os.path.split(path)[-1]
-                    copyfile(path, os.path.join('./out/ex/', f'{i}_{fn}'))
-                exit()
+                found_image_idxs = search(searched_image, blanks=get_masked_areas(mask))
+                found_images = dataset.images[found_image_idxs[:23]]
+                paths = dataset.paths[[searched_image_idx] + list(found_image_idxs[:40])]
 
+                # vutils.save_image(mask, 'default_mask.jpg')
+                # for i, path in enumerate(paths):
+                #     fn = os.path.split(path)[-1]
+                #     copyfile(path, os.path.join('./out/ex/', f'{i}_{fn}'))
 
                 print('Searching nearest', time() - t0)
 
-                vutils.save_image(dataset.images[[new_idx] + list(idxs[:23])],
-                                    '%s/%s_test_naive.png' % (args.output_dir, i),
-                                    normalize=True)
+                vutils.save_image(torch.cat([searched_image, found_images], dim=0),
+                                '%s/%s_test_naive.png' % (args.output_dir, i),
+                                normalize=True)
+
+                get_prox_mask = get_prox_op(im_size)
+                gabor_conv = build_gabor_conv(im_size)
+                prox_mask = get_prox_mask(mask)
+
+                for found_image in found_images:
+                    best_t, (mi, mj) = best_translation(
+                        searched_image[0],
+                        found_image,
+                        prox_mask,
+                        image_transform=gabor_conv,
+                        punition=lambda x,y: 5 * (x + y)
+                    )
+
+                    print(mi, mj)
+
+
         if args.lsh:
             print('Building LSH')
             t0 = time()
@@ -99,17 +118,17 @@ if __name__ == '__main__':
             for i in range(20):
                 t0 = time()
 
-                new_idx = rd.randint(int(len(vr) * 0.8), len(vr) - 1)
-                new_image = dataset.images[new_idx]
+                searched_image_idx = rd.randint(int(len(vr) * 0.8), len(vr) - 1)
+                new_image = dataset.images[searched_image_idx]
                 m = datasketch.MinHash(num_perm=128)
                 m.update(pack(f'{vr._vector_size}f', *gist(new_image)[0]))
-                idxs = lsh.query(m)
+                found_image_idxs = lsh.query(m)
 
                 print('Searching nearest', time() - t0)
 
-                print(new_idx)
-                print(idxs)
+                print(searched_image_idx)
+                print(found_image_idxs)
 
-                vutils.save_image(torch.stack(new_image + [dataset[idx][0] for idx in idxs[:23]], 0),
+                vutils.save_image(torch.stack(new_image + [dataset[idx][0] for idx in found_image_idxs[:23]], 0),
                                     '%s/%s_test_lsh.png' % (args.output_dir, i),
-                                    normalize=True)
+                                  normalize=True)
